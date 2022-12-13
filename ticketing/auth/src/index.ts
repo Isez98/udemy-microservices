1e756071fcd2edd7b08855d485002aa53bd6a945
Isacc Hernandez <isacc1798@gmail.com>
2022-12-12 20:15:00 -0700

    [ADD] Jest testing & first test

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,38 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
-// routes
-import { currentUserRouter } from './routes/current-user'
-import { signInRouter } from './routes/signIn'
-import { signOutRouter } from './routes/signOut'
-import { signUpRouter } from './routes/signUp'
 // database ODCB
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-// error handler
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
