9fe82fa62dc2057b7ab52bcf3d83bd51f08a097c
Isacc Hernandez <isacc1798@gmail.com>
2023-03-07 21:25:24 -0700

    Change domain host

--- ticketing/client/api/buildClient.js
+++ ticketing/client/api/buildClient.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://www.isez.dev',
+      baseURL: 'http://www.ticketing.isez.dev',
       headers: req.headers,
     })
   } else {
